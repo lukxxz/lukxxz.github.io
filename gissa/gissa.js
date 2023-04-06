7014a64dcfc8a7ece7548f06efcce6b5cc782caa
Lukas Kristensson Blom <lukr0903@storsjogymnasiet.se>
2023-04-06 10:57:21 +0200

    nästan klar

---
+++ gissa/gissa.js
@@ -0,0 +1,15 @@
+const slumptal = Math.floor(Math.random()* 9) + 1;
+let antalgissningar = 0
+
+console.log("slumptalet " + slumptal)
+
+function gissatal() {
+
+    console.log("click")
+let textfelt_gissa = document.getElementById("gissa").value 
+let gissa_int = Number(textfelt_gissa)
+let vinn = false
+let p_svar = document.getElementById("p_svar")
+
+console.log("textfelt=" + textfelt_gissa)
+}
